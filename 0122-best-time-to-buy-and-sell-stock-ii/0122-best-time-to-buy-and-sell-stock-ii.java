ac8560689553f68755e89c39a7912c8d943cbf71
SYED NOWROSE <syednowrose3@gmail.com>
2026-07-18 19:49:45 +0530

    Time: 0 ms (100%), Space: 46.5 MB (32.45%) - LeetHub

---
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+
+        int profit = 0;
+
+        for(int i = 1; i < prices.length; i++) {
+            if(prices[i] > prices[i-1]) {
+                profit += prices[i] - prices[i-1];
+            }
+        }
+
+        return profit;
+    }
+}
+
+class Main{
+    public static void main(String[] args) {
+        int[] prices1 ={ 7,1,5,3,6,4};
+        int[] prices2 ={7,6,4,3,1};
+
+        Solution s = new Solution();
+        System.out.println(s.maxProfit(prices1));
+        System.out.println(s.maxProfit(prices2));
+
+    }
+}
